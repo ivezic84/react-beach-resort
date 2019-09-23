f77b7b1468fb34aad9b85b71ed8aa4f688dd0f58
unknown <ivezicljubisa@gmail.com>
2019-09-23 19:38:10 +0200

    .env.development

--- src/contentful.js
+++ src/contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "3xlnx5uitq0w",
-  accessToken: "Jhf8UcVRVhfOCssTgNE_JYLPjDKAAEJpUkapzhsK7L4"
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_API_ACCESS_TOKEN
 });
